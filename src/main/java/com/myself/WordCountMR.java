5d7e84fc6f9b3c28e478ce874308e5fbb60cabcb
sjs <shijs@yonyou.com>
2018-12-03 17:52:59 +0800

    start

--- src/main/java/com/myself/WordCountMR.java
+++ src/main/java/com/myself/WordCountMR.java
@@ -1,13 +1,38 @@
 package com.myself;
 
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 /**
- * Hello world!
+ * Word Count MarReduce Application
  *
  */
 public class WordCountMR
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    public static class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+        private final IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(LongWritable key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            StringTokenizer st = new StringTokenizer(value.toString());
+            while (st.hasMoreTokens()) {
+                word.set(st.nextToken());
+                context.write(word, one)
+            }
+        }
     }
+
+    //2. Reducer 类
+    public static class WorkCountReducer extends Reducer<KEYIN, VALUEIN, KEYOUT, VALUEPUT>;
+
+    //3. Job 任务
+    public int run(String[] strings) throws Exception;
+
 }
