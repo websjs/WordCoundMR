edc5986a1afb5d2a9f8356a307edf139a1f82094
sjs <shijs@yonyou.com>
2018-12-03 16:40:19 +0800

    init

---
+++ src/main/java/com/myself/WordCountMR.java
@@ -0,0 +1,13 @@
+package com.myself;
+
+/**
+ * Hello world!
+ *
+ */
+public class WordCountMR
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/myself/AppTest.java
@@ -0,0 +1,20 @@
+package com.myself;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple WordCountMR.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
